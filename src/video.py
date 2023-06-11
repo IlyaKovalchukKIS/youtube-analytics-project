fe2385e7179d73c98126078812df2999bc66bd69
ilya <mrlivedance@vk.com>
2023-06-11 12:37:23 +0300

    в инициализацию класса Video добавлена проверка на корректность id видео с YouTube

--- src/video.py
+++ src/video.py
@@ -1,6 +1,7 @@
 import os
 
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -18,13 +19,24 @@ class Video:
 
         """
         self.video_id: str = video_id
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=self.video_id
-                                                    ).execute()
-        self.title: str = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=self.video_id
+                                                        ).execute()
+            if not video_response['items']:
+                raise ValueError("Incorrect Video ID")
+
+            self.title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
+        except (HttpError, ValueError) as e:  # Ошибка подключения к YouTube Data API или некорректный Video ID
+            print(f"Error: {e}")
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self) -> str:
         """Магический метод (__str__) который выводит название видео"""
